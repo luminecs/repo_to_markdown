a763abc2926fd526a283e846f3a69cac8bc12192
luminecs <wangxuke.china@gmail.com>
2025-04-01 16:36:14 +0800

    commit

---
+++ bin/repo_to_markdown.dart
@@ -0,0 +1,5 @@
+import 'package:repo_to_markdown/repo_to_markdown.dart' as repo_to_markdown;
+
+void main(List<String> arguments) {
+  print('Hello world: ${repo_to_markdown.calculate()}!');
+}
---
+++ test/repo_to_markdown_test.dart
@@ -0,0 +1,8 @@
+import 'package:repo_to_markdown/repo_to_markdown.dart';
+import 'package:test/test.dart';
+
+void main() {
+  // test('calculate', () {
+  //   expect(calculate(), 42);
+  // });
+}
